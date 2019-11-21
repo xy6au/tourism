33ccf3c4f64262fc79786699d4279e89c95a3ae2
xy6au <56407106+xy6au@users.noreply.github.com>
2019-11-21 14:00:21 -0500

    initial commit

---
+++ Carousel Template for Bootstrap_files/map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken='pk.eyJ1IjoieHk2YXUiLCJhIjoiY2sxdXlwejk5MGZ5aTNncXV3MGx0Y3JmcCJ9.2LvX99rC6vL08hPV1pVhxg';
+var map=new mapboxgl.Map({
+container:'map',
+style:'mapbox://styles/xy6au/ck2135n2m3tev1cmd1oq8zk2a'
+
+});
---
+++ map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken='pk.eyJ1IjoieHk2YXUiLCJhIjoiY2sxdXlwejk5MGZ5aTNncXV3MGx0Y3JmcCJ9.2LvX99rC6vL08hPV1pVhxg';
+var map=new mapboxgl.Map({
+container:'map',
+style:'mapbox://styles/xy6au/ck2o2wgh30cdd1bpyhr3o5e2z'
+
+});
